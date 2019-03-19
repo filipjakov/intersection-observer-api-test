1d065f5d459a3331b2bea999281f213879ca5680
fbulic <filip-jakov.bulic@fer.hr>
2019-03-20 00:52:46 +0100

    Fix removed unecessary dummy data endpoint

--- src/dummy-data/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // res.writeHead(200, {"Content-Type": "application/json"});
-
-  const images = [...Array(20).keys()].map(_ => 'https://via.placeholder.com/600x200');
-  console.log(images);
-  res.send({ images });
-};
