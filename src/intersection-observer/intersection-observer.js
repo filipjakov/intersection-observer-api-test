747ef01b064d15aa3a0ecdf8f5152dc83b5b18ff
fbulic <filip-jakov.bulic@fer.hr>
2019-03-20 23:34:20 +0100

    Fix pre presentation

--- src/intersection-observer/intersection-observer.js
+++ src/intersection-observer/intersection-observer.js
@@ -1,13 +1,24 @@
 const numberOfImages = 50;
-const imageHeight = 600;
-const imageWidth = 400;
+const imageHeight = 300;
+const imageWidth = 200;
 const url = `https://via.placeholder.com/${imageHeight}x${imageWidth}`;
 
 // Generate an array of image links
 const images = new Array(numberOfImages).fill(url)
 
 // For each image link, create img element, wrap it in a div and insert it in the DOM
-const targets = images.map(() => {
+const targets = images.map((_, i) => {
+  if (i === 0) {
+    const img = document.createElement('img');
+    const container = document.createElement('div');
+
+    container.append(img);
+    container.classList.add('space');
+
+    document.body.insertBefore(container, document.getElementsByTagName('p')[1]);
+    return img;
+  }
+  
   const img = document.createElement('img');
   const container = document.createElement('div');
 
@@ -18,16 +29,26 @@ const targets = images.map(() => {
   return img;
 });
 
+
 // Take dom element as argument
 const cb = img => {
-  // entries - observations on the element
-  // observer - manage the actuall instance of observer
+
+  // IO options
+  const options = {
+    // root: null,
+    rootMargin: '-100px 0px -100px 0px', // default "top right bottom left" - "0px 0px 0px 0px"
+    threshold: 1.0, // default threshold of 0.0. -> might be list of values
+  }
+
+  // entries  - Array<IntersectionObserverEntry> -> each representing one threshold which was crossed, 
+  // observer - IntersectionObserver -> The IntersectionObserver for which the callback is being invoked.
   const io = new IntersectionObserver((entries, observer) => {
+    // In this demo is always one entry
     entries.forEach((entry, i) => {
       const header = document.getElementsByClassName('header__content-text')[0];
       header.innerHTML = (Number(header.innerHTML) + 1).toString();
 
-      // No calculation!!!
+      // No calculation needed!!!
       if (entry.isIntersecting) {
         const img = entry.target;
 
@@ -37,13 +58,9 @@ const cb = img => {
         observer.disconnect();
       }
     });
-  });
+  }, options);
 
   io.observe(img);
 };
 
-// TODO: https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserverEntry
-// TODO: https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver
-// TODO: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 targets.forEach(cb);
-
--- src/naive/naive.js
+++ src/naive/naive.js
@@ -1,13 +1,27 @@
-const numberOfImages = 50;
+const numberOfImages = 25;
 const imageHeight = 600;
 const imageWidth = 400;
 const url = `https://via.placeholder.com/${imageHeight}x${imageWidth}`;
 
 // Generate an array of image links
 const images = new Array(numberOfImages).fill(url)
+// Sretni uhljebic
+images[images.length - 1] = 'https://avatars1.githubusercontent.com/u/9164266?s=460&v=4';
 
 // For each image link, create img element, wrap it in a div and insert it in the DOM
-const targets = images.map(() => {
+// Store references on img elements
+const targets = images.map((_, i) => {
+  if (i === 0) {
+    const img = document.createElement('img');
+    const container = document.createElement('div');
+
+    container.append(img);
+    container.classList.add('space');
+
+    document.body.insertBefore(container, document.getElementsByTagName('p')[1]);
+    return img;
+  }
+
   const img = document.createElement('img');
   const container = document.createElement('div');
 
